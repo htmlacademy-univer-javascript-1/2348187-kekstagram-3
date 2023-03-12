06a02de1809edbcc3490e1d5a099036d577801d2
susemaa <rad.archeg@gmail.com>
2023-03-12 23:48:39 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function getRand(min, max) {
   if (min < 0 || max < 0) {
-    console.log('nums are lower than 0');
+    alert('nums are lower than 0');
     return '';
   }
-if (min > max) {
-  console.log('min > max');
-  return '';
-}
-if (min === max) {
-  return max;
-}
+  if (min > max) {
+    alert('min > max');
+    return '';
+  }
+  if (min === max) {
+   return max;
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -20,3 +20,8 @@ function lenghtCheck(str, maxLen) {
 
   return false;
 }
+function main(){
+  alert(getRand(1, 10));
+  alert('keksik kotik', 25);
+}
+
